0c48f6b6408e7497feb784c9039612e4fd865a41
michele camera <michy.cam95@gmail.com>
2022-06-24 19:15:29 +0200

    modify scss

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,52 +1,37 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-10  border-bottom">
+    <div class="row d-flex flex-column">
+        <div class=" col-12   border-bottom d-flex justify-content-center ">
             <h1>
-                Tutti gli Appartamenti
+                Appartamenti
             </h1>
         </div>
-        <div class="col-2 pb-2 border-bottom d-flex justify-content-end">
+        <div class=" col-12 p-2  border-bottom d-flex justify-content-center">
             <div>
-                <a class="btn btn-primary" href="{{route('admin.apartments.create')}}">Crea nuovo appartamento</a>
+                <a class="btn btn-insert " href="{{route('admin.apartments.create')}}">Inserisci</a>
             </div>
         </div>
     </div>
-    <div class="row py-2 ">
-        <div class="col-3 border-bottom">
-            <h4>ID</h4>
-        </div>
-        <div class="col-3 border-bottom">
-            <h4>Titolo</h4>
-        </div>
-        <div class="col-3 border-bottom">
-            <h4>Slug</h4>
-        </div>
-        <div class="col-3 border-bottom">
-            <h4>Azioni</h4>
-        </div>
-    </div>
-    <div class="row py-2 ">
+    <div class="row">
         @foreach ($apartments as $apartment)
-        <div class="col-3 border-bottom">
-            <p>{{$apartment->id}}</p>
+        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 p-2 d-flex align-items-center  border-bottom">
+            <img src="{{asset('/storage/'. $apartment->img)}}" alt="immagine">
         </div>
-        <div class="col-3 border-bottom">
-            <p>{{$apartment->title}}</p>
+        <div class="col-xs-3 col-sm-2 col-md-3 col-lg-3 p-2 d-flex align-items-center  border-bottom">
+            <p class="m-0">{{$apartment->title}}</p>
         </div>
-        <div class="col-3 border-bottom">
-            <p>{{$apartment->slug}}</p>
+        <div class="col-xs-3 col-sm-2 col-md-3 col-lg-3 p-2 d-flex align-items-center  border-bottom">
+            <p class="m-0">{{$apartment->address}}</p>
         </div>
-        <div class="col-3 border-bottom">
-            <a href="{{route('admin.apartments.show', $apartment->id)}}">show </a>/
-            <a href="{{route('admin.apartments.edit', $apartment->id)}}">edit </a>/
-            <form class="d-inline" action="{{route('admin.apartments.destroy',$apartment->id)}}" method="POST">
+        <div class="pt-2 pb-2 col-xs-3 col-sm-3 col-md-3 col-lg-2 flex-md-column d-flex align-items-center justify-content-center  border-bottom">
+            <button class='btn btn-primary w-100' href="{{route('admin.apartments.show', $apartment->id)}}">DETTAGLI </button>
+            <button class='m-1 btn btn-secondary w-100' href="{{route('admin.apartments.edit', $apartment->id)}}">MODIFICA </button>
+            <form class="d-inline  w-100" action="{{route('admin.apartments.destroy',$apartment->id)}}" method="POST">
                 @csrf
                 @method('DELETE')
                 <input name="_method" type="hidden" value="DELETE">
-
-                <button class="border-0 text-primary bg-transparent" onclick="return confirm('Are your sure?')" type="submit">remove</button>
+                <button class="btn btn-danger w-100 " onclick="return confirm('Are your sure?')" type="submit">CANCELLA</button>
             </form>
         </div>
         @endforeach
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -23,16 +23,11 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-md flex-md-nowrap p-0">
+    <nav class="navbar navbar-expand-md flex-md-nowrap mb-4 p-0">
         <div class="cnt-logo">
             <img src="{{url('/img/boolbnb-logo.svg')}}" alt="logo">
         </div>
         <ul class="navbar-nav px-3 ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('admin.apartments.index') }}">
-                    Visita il sito
-                </a>
-            </li>
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
                                  document.getElementById('logout-form').submit();">
@@ -46,20 +41,20 @@
     </nav>
     <div class="container-fluid">
         <div class="row">
-            <nav class="col-md-2 d-none d-md-block bg-light sidebar py-4">
+            <nav class="col-sd-2 col-md-2 d-md-block  sidebar py-4 nav-sidebar-home">
                 <div class="sidebar-sticky">
                     <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="{{ route('admin.apartments.index') }}">
+                        <li class="nav-item  ">
+                            <a class="nav-link active text-left d-flex align-items-center  " href="{{ route('admin.apartments.index') }}">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home">
                                     <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
                                     <polyline points="9 22 9 12 15 12 15 22"></polyline>
                                 </svg>
-                                Appartamenti
+                                <span>Appartamenti</span>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">
+                            <a class="nav-link ml-1 text-left d-flex align-items-center " href="#">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
                                     <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
                                     <polyline points="14 2 14 8 20 8"></polyline>
@@ -67,28 +62,28 @@
                                     <line x1="16" y1="17" x2="8" y2="17"></line>
                                     <polyline points="10 9 9 9 8 9"></polyline>
                                 </svg>
-                                Sponsor
+                                <span>Sponsor</span>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
+                        <li class="nav-item ">
+                            <a class="nav-link ml-1 text-left d-flex align-items-center " href="#">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users">
                                     <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
                                     <circle cx="9" cy="7" r="4"></circle>
                                     <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
                                     <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
                                 </svg>
-                                Messaggi
+                                <span>Messaggi</span>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
+                        <li class="nav-item ">
+                            <a class="nav-link text-left  d-flex align-items-center" href="#">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-activity">
                                     <polyline points="4 7 4 4 20 4 20 7"></polyline>
                                     <line x1="9" y1="20" x2="15" y2="20"></line>
                                     <line x1="12" y1="4" x2="12" y2="20"></line>
                                 </svg>
-                                Statistiche
+                                <span> Statistiche</span>
                             </a>
                         </li>
                     </ul>
