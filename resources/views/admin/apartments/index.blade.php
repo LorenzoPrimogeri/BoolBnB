1725992278f3c966b073d0e859b51207b803784e
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-06-24 20:31:33 +0200

    important comit

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container">
     <div class="row d-flex flex-column">
-        <div class=" col-12    d-flex justify-content-center ">
+        <div class=" col-12 d-flex justify-content-center ">
             <h1>
                 Appartamenti
             </h1>
@@ -16,7 +16,7 @@
     <div class="row">
         @foreach ($apartments as $apartment)
         <div class="col-xs-3 col-sm-3 col-md-3 col-lg-2 p-2 d-flex align-items-center  border-bottom">
-            <img src="{{asset('/storage/'. $apartment->img)}}" alt="immagine">
+            <img src="{{asset('storage/'. $apartment->img)}}" alt="immagine">
         </div>
         <div class="col-xs-3 col-sm-2 col-md-3 col-lg-4 p-2 d-flex align-items-center  border-bottom">
             <p class="m-0">{{$apartment->title}}</p>
@@ -24,16 +24,20 @@
         <div class="col-xs-3 col-sm-2 col-md-3 col-lg-4 p-2 d-flex align-items-center  border-bottom">
             <p class="m-0">{{$apartment->address}}</p>
         </div>
-        <div class="pt-2 pb-2 col-xs-3 col-sm-3 col-md-3 col-lg-2 flex-md-column d-flex align-items-center justify-content-center  border-bottom">
+        <div
+            class="pt-2 pb-2 col-xs-3 col-sm-3 col-md-3 col-lg-2 flex-md-column d-flex align-items-center justify-content-center  border-bottom">
             <a class='btn btn-primary w-100' href="{{route('admin.apartments.show', $apartment->id)}}">DETTAGLI </a>
-            <a class='m-1 btn btn-secondary w-100' href="{{route('admin.apartments.edit', $apartment->id)}}">MODIFICA </a>
+            <a class='m-1 btn btn-secondary w-100' href="{{route('admin.apartments.edit', $apartment->id)}}">MODIFICA
+            </a>
             <form class="d-inline  w-100" action="{{route('admin.apartments.destroy',$apartment->id)}}" method="POST">
                 @csrf
                 @method('DELETE')
                 <input name="_method" type="hidden" value="DELETE">
-                <button class="btn btn-danger w-100 " onclick="return confirm('Are your sure?')" type="submit">CANCELLA</button>
+                <button class="btn btn-danger w-100 " onclick="return confirm('Are your sure?')"
+                    type="submit">CANCELLA</button>
             </form>
         </div>
-
+        @endforeach
     </div>
+</div>
 @endsection
