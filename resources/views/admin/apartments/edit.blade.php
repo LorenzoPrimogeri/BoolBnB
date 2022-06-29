b3171cf3a01d3049c91638bdf4c836c1099069d8
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-06-29 19:53:07 +0200

    fix

--- resources/views/admin/apartments/edit.blade.php
+++ resources/views/admin/apartments/edit.blade.php
@@ -1,106 +1,114 @@
 @extends('layouts.app')
 @section('content')
-    <div class="main-slct-item">
-        <div class="cnt-slct-btn">
-            <div class="cnt-th cnt-btn h-100">
-                <a href="{{ route('admin.apartments.index') }}">
-                    <button>Appartamenti</button>
-                </a>
-            </div>
+<div class="main-slct-item">
+    <div class="cnt-slct-btn">
+        <div class="cnt-th cnt-btn h-100">
+            <a href="{{ route('admin.apartments.index') }}">
+                <button>Appartamenti</button>
+            </a>
         </div>
-        <div class="cnt-item-details">
-            <div>
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
+    </div>
+    <div class="cnt-item-details">
+        <div>
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
             </div>
-            <form action="{{ route('admin.apartments.update', $apartment->id) }}" method="POST"
-                enctype="multipart/form-data">
-                @csrf
-                @method('PUT')
-                <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
-                    <h3>Visibilità:</h3>
-                    <select class="form-select" aria-label="Default select example" name="visible">
-                        <option value="{{ $apartment->visible }}">
-                            {{ $apartment->visible ? 'Visibile' : 'Non visibile' }}
-                        </option>
-                        @if ($apartment->visible == 1)
-                            <option value="0">
-                                {{ !$apartment->visible ? 'Visibile' : 'Non visibile' }}
-                            </option>
-                        @else
-                            <option value="1">
-                                {{ !$apartment->visible ? 'Visibile' : 'Non visibile' }}
-                            </option>
-                        @endif
-                    </select>
+            @endif
+        </div>
+        <form action="{{ route('admin.apartments.update', $apartment->id) }}" method="POST"
+            enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
+                <h3>Visibilità:</h3>
+                <select class="form-select" aria-label="Default select example" name="visible">
+                    <option value="{{ $apartment->visible }}">
+                        {{ $apartment->visible ? 'Visibile' : 'Non visibile' }}
+                    </option>
+                    @if ($apartment->visible == 1)
+                    <option value="0">
+                        {{ !$apartment->visible ? 'Visibile' : 'Non visibile' }}
+                    </option>
+                    @else
+                    <option value="1">
+                        {{ !$apartment->visible ? 'Visibile' : 'Non visibile' }}
+                    </option>
+                    @endif
+                </select>
+            </div>
+            <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
+                <h3>Titolo:</h3>
+                <input class="w-100 p-1" type="text" name="title" placeholder="titolo" value="{{ $apartment->title }}"
+                    required>
+            </div>
+            <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
+                <h3>Descrizione:</h3>
+                <textarea name="description" class="w-100" rows="10" placeholder="descrizione"
+                    required>{{ $apartment->description }}</textarea>
+            </div>
+            <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
+                <div class="cnt-img">
+                    <img src="{{ asset('storage/' . $apartment->img) }}" alt="">
+                    <input type="file" name="img">
                 </div>
-                <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
-                    <h3>Titolo:</h3>
-                    <input class="w-100 p-1" type="text" name="title" placeholder="titolo"
-                        value="{{ $apartment->title }}" required>
-                </div>
-                <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
-                    <h3>Descrizione:</h3>
-                    <textarea name="description" class="w-100" rows="10" placeholder="descrizione" required>{{ $apartment->description }}</textarea>
-                </div>
-                <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
-                    <div class="cnt-img">
-                        <img src="{{ asset('storage/' . $apartment->img) }}" alt="">
-                        <input type="file" name="img">
-                    </div>
-                </div>
-                <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
-                    <div class="autocomplete">
-                        <h3>Indirizzo: {{ $apartment->address }}</h3>
-                        {{-- <input class="w-100 p-1" type="text" name="address" placeholder="indirizzo"
+            </div>
+            <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
+                <div class="autocomplete">
+                    <h3>Indirizzo: {{ $apartment->address }}</h3>
+                    {{-- <input class="w-100 p-1" type="text" name="address" placeholder="indirizzo"
                         value="{{ $apartment->address }}" required> --}}
-                        <input type="hidden" name="address" value="{{ $apartment->address }}" id="inputValue">
+                    <input type="hidden" name="address" value="{{ $apartment->address }}" id="inputValue">
+                </div>
+            </div>
+            <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
+                <h3>Stanze:</h3>
+                <input class="w-100 p-1" type="number" name="room" placeholder="numero di stanze"
+                    value="{{ $apartment->room }}" required>
+            </div>
+            <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
+                <h3>Bagni:</h3>
+                <input class="w-100 p-1" type="number" name="bathroom" placeholder="numero di bagni"
+                    value="{{ $apartment->bathroom }}" required>
+            </div>
+            <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
+                <h3>Letti:</h3>
+                <input class="w-100 p-1" type="text" name="bed" placeholder="numero di letti"
+                    value="{{ $apartment->bed }}" required>
+            </div>
+            <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
+                <h3>Metri Quadri:</h3>
+                <input class="w-100 p-1" type="number" name="mq" placeholder="numero di metri quadri"
+                    value="{{ $apartment->mq }}" required>
+            </div>
+            <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
+                <h3>Prezzo:</h3>
+                <input class="w-100 p-1" type="text" name="price" placeholder="prezzo" value="{{ $apartment->price }}"
+                    required>
+            </div>
+            <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
+                <div class="cnt-service">
+                    <div class="col-1">
+                        <h3>Servizi:</h3>
                     </div>
                 </div>
-                <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
-                    <h3>Stanze:</h3>
-                    <input class="w-100 p-1" type="number" name="room" placeholder="numero di stanze"
-                        value="{{ $apartment->room }}" required>
-                </div>
-                <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
-                    <h3>Bagni:</h3>
-                    <input class="w-100 p-1" type="number" name="bathroom" placeholder="numero di bagni"
-                        value="{{ $apartment->bathroom }}" required>
-                </div>
-                <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
-                    <h3>Letti:</h3>
-                    <input class="w-100 p-1" type="text" name="bed" placeholder="numero di letti"
-                        value="{{ $apartment->bed }}" required>
-                </div>
-                <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
-                    <h3>Metri Quadri:</h3>
-                    <input class="w-100 p-1" type="number" name="mq" placeholder="numero di metri quadri"
-                        value="{{ $apartment->mq }}" required>
-                </div>
-                <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
-                    <h3>Prezzo:</h3>
-                    <input class="w-100 p-1" type="text" name="price" placeholder="prezzo"
-                        value="{{ $apartment->price }}" required>
-                </div>
-                <div class="cnt-service">
-                    <h3>Servizi:</h3>
+                <div class="cnt-items-services col-11">
                     @foreach ($services as $service)
-                        <input type="checkbox" value="{{ $service->id }}" name="services[]"
-                            {{ $apartment->services->contains($service) ? 'checked' : '' }}>
+                    <div class="cnt-item-service">
+                        <input type="checkbox" value="{{ $service->id }}" name="services[]" {{
+                            $apartment->services->contains($service) ? 'checked' : '' }}>
                         <span class="form-check-label">{{ $service->name }}</span>
+                    </div>
                     @endforeach
                 </div>
                 <div class="cnt-row col-12 flex-dr-col algn-itm-init b-0">
                     <button class="btn-modify" type="submit">Salva modifiche</button>
                 </div>
-            </form>
-        </div>
+        </form>
     </div>
+</div>
 @endsection
--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -1,72 +1,69 @@
 @extends('layouts.app')
 @section('content')
-    <div class="main-slct-item">
-        <div class="cnt-slct-btn">
-            <div class="cnt-th cnt-btn h-100">
-                <a href="{{ route('admin.apartments.edit', $apartment->id) }}">
-                    <button>Modifica</button>
-                </a>
-                <form class="d-inline  w-100" action="{{ route('admin.apartments.destroy', $apartment->id) }}"
-                    method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <input name="_method" type="hidden" value="DELETE">
-                    <button class="btn btn-danger w-100 " onclick="return confirm('Are your sure?')"
-                        type="submit">Elimina</button>
-                </form>
-                <a href="{{ route('admin.apartments.index') }}">
-                    <button>Appartamenti</button>
-                </a>
-            </div>
-        </div>
-        <div class="cnt-item-details">
-            <div class="cnt-row col-12">
-                <h3>Visibilità:</h3>
-                <p class="w-100 p-1"> {{ $apartment->visible ? 'visibile al momento' : 'Non visibile' }} </p>
-            </div>
-            <div class="cnt-row col-12">
-                <h3>Titolo:</h3>
-                <p>{{ $apartment->title }}</p>
-            </div>
-            <div class="cnt-row col-12">
-                <h3>Indirizzo:</h3>
-                <p>{{ $apartment->address }}</p>
-            </div>
-            <div class="cnt-row col-12">
-                <div class="cnt-img">
-                    <img src="{{ asset('storage/' . $apartment->img) }}" alt="">
-                </div>
-            </div>
-            <div class="cnt-row col-12">
-                <h3>Descrizione:</h3>
-                <p>{{ $apartment->description }}</p>
-            </div>
-            <div class="cnt-row col-12">
-                <h3>Stanze:</h3>
-                <p>{{ $apartment->room }}</p>
-            </div>
-            <div class="cnt-row col-12">
-                <h3>Bagni:</h3>
-                <p>{{ $apartment->bathroom }}</p>
-            </div>
-            <div class="cnt-row col-12">
-                <h3>Letti:</h3>
-                <p>{{ $apartment->bed }}</p>
-            </div>
-            <div class="cnt-row col-12">
-                <h3>Metri Quadri:</h3>
-                <p>{{ $apartment->mq }}</p>
-            </div>
-            <div class="cnt-row col-12">
-                <h3>Prezzo:</h3>
-                <p>{{ $apartment->price }}</p>
-            </div>
-            <div class="cnt-row col-12">
-                <h3>Servizi:</h3>
-                @foreach ($apartment->services as $service)
-                    <p>{{ $service->name }} </p>
-                @endforeach
-            </div>
+<div class="main-slct-item">
+    <div class="cnt-slct-btn">
+        <div class="cnt-th cnt-btn h-100">
+            <a href="{{ route('admin.apartments.edit', $apartment->id) }}">
+                <button>Modifica</button>
+            </a>
+            <form class="d-inline wmax-100" action="{{ route('admin.apartments.destroy', $apartment->id) }}"
+                method="POST">
+                @csrf
+                @method('DELETE')
+                <input name="_method" type="hidden" value="DELETE">
+                <button class="btn btn-danger wmax-100" onclick="return confirm('Are your sure?')"
+                    type="submit">Elimina</button>
+            </form>
         </div>
     </div>
+    <div class="cnt-item-details">
+        <div class="cnt-row col-12">
+            <h3>Visibilità:</h3>
+            <p class="w-100 p-1"> {{ $apartment->visible ? 'visibile al momento' : 'Non visibile' }} </p>
+        </div>
+        <div class="cnt-row col-12">
+            <h3>Titolo:</h3>
+            <p>{{ $apartment->title }}</p>
+        </div>
+        <div class="cnt-row col-12">
+            <h3>Indirizzo:</h3>
+            <p>{{ $apartment->address }}</p>
+        </div>
+        <div class="cnt-row col-12">
+            <div class="cnt-img">
+                <img src="{{ asset('storage/' . $apartment->img) }}" alt="">
+            </div>
+        </div>
+        <div class="cnt-row col-12">
+            <h3>Descrizione:</h3>
+            <p>{{ $apartment->description }}</p>
+        </div>
+        <div class="cnt-row col-12">
+            <h3>Stanze:</h3>
+            <p>{{ $apartment->room }}</p>
+        </div>
+        <div class="cnt-row col-12">
+            <h3>Bagni:</h3>
+            <p>{{ $apartment->bathroom }}</p>
+        </div>
+        <div class="cnt-row col-12">
+            <h3>Letti:</h3>
+            <p>{{ $apartment->bed }}</p>
+        </div>
+        <div class="cnt-row col-12">
+            <h3>Metri Quadri:</h3>
+            <p>{{ $apartment->mq }}</p>
+        </div>
+        <div class="cnt-row col-12">
+            <h3>Prezzo:</h3>
+            <p>{{ $apartment->price }}</p>
+        </div>
+        <div class="cnt-row col-12">
+            <h3>Servizi:</h3>
+            @foreach ($apartment->services as $service)
+            <p>{{ $service->name }} </p>
+            @endforeach
+        </div>
+    </div>
+</div>
 @endsection
