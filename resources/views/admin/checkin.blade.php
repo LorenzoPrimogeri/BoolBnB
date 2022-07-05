e80b120bc154a6c8913da39c591f27a960e03404
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-07-05 23:26:35 +0200

    adding payment

---
+++ resources/views/admin/checkin.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+{{-- @dd($user); --}}
+@section('content')
+
+<div style="margin: 500px;">
+    <div>
+        <div>
+            {{-- title --}}
+            <h5>...</h5>
+            @foreach ($sponsors as $sponsor)
+            <ul>
+                <li>
+                    <a href="{{ route('admin.checkout',$sponsor->id) }}">
+                        <h5>Type: {{ $sponsor->type }}</h5>
+
+                        <span>Duration: {{ $sponsor->duration }}</span>
+                    </a>
+                </li>
+                <li>Price: {{ $sponsor->price }}</li>
+            </ul>
+            @endforeach
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use Carbon\Carbon;
+use App\Apartment;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
 use App\Http\Controllers\MessageController;
+use App\Sponsorship;
+use Braintree\Gateway;
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,8 +31,29 @@ Route::middleware('auth')
         Route::get('/', 'HomeController@index')->name('home');
         Route::resource('/apartments', 'ApartmentController');
         Route::resource('apartments.messages', "MessageController");
-        Route::get('/messages','MessageController@index')->name('messages');
-
+        Route::get('/messages', 'MessageController@index')->name('messages');
+        Route::get("/checkin", "ApartmentController@sponsors")->name("checkin");
+        Route::get("/checkout/{id}", "ApartmentController@checkOut")->name("checkout");
+        Route::get('/confirmed', function () {
+            return view('admin.confirmed');
+        })->name('confirmed');
+        Route::put('/payment', function (Request $request, Gateway $gateway) {
+            $idSponsor = $request->id;
+            $sponsor = Sponsorship::find($idSponsor);
+            //   $apartment = Apartment::find(Apartment::id());
+            //  $activeSponsor = $apartment->sponsorShips;
+            $amount = $sponsor->price;
+            $nonce = $request->payment_method_nonce;
+            // $sponsorshipName = $sponsor->type;
+            $result = $gateway->transaction()->sale([
+                'amount' => $amount,
+                'paymentMethodNonce' => $nonce,
+                'options' => [
+                    'submitForSettlement' => true
+                ]
+            ]);
+            return view('admin.confirmed', ['sponsor' => $sponsor]);
+        })->name('payment');
     });
 
 
