05c4b0c2d928cc8b62f57426366ea6e1f097d825
MarkCat-11 <lionmarco11@gmail.com>
2022-07-05 18:04:24 +0200

    fix search home, add style search-result

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -15,7 +15,7 @@
           </div>
           <div class="cnt-nav col-8 h-100">
             <!--search-->
-            <div class="search" v-show="$route.name === 'home' ? true : false">
+            <div class="search">
               <div class="cnt-lens"></div>
               <div class="contStringSrc">
                 <input
@@ -99,7 +99,11 @@
         class="cnt-items"
         v-for="(indirizzo, i) in indirizzi"
         :key="i + indirizzo.address"
-      ></div>
+      >
+        <a href="#" @click="take(indirizzo.address.freeformAddress)">
+          {{ indirizzo.address["freeformAddress"] }}
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -147,7 +151,7 @@ export default {
         { params: { query: this.input } }
       ).then((risp) => {
         const risultati = risp.data.results;
-        // console.log(risultati);
+        console.log(risultati);
         this.indirizzi = risultati;
       });
       return this.indirizzi;
@@ -155,104 +159,10 @@ export default {
     take(indirizzo) {
       this.input = indirizzo;
     },
-    takeLatLng() {
-      //prendo lat e long dal indirizzo
-      console.log(this.input);
-      Axios.get(
-        "https://api.tomtom.com/search/2/geocode/.json?storeResult=false&limit=1&view=Unified&key=GpuJFPNSTUcwZDlHR1mIhVAs6Z457GsK",
-        { params: { query: this.input } }
-      ).then((risp) => {
-        const position = risp.data.results[0].position;
-        this.lat = position.lat;
-        this.lng = position.lon;
-        // console.log("lat:" + this.lat + " lng:" + this.lng);
-        this.searchApartments();
-      });
-    },
-    searchApartments() {
-      // console.log(this.services);
-      //reset degli appartamenti corretti
-      this.correctApartments = [];
-      console.log(this.correctApartments);
-      //console.log(this.apartaments);
-      for (let i = 0; i < this.allApartaments.length; i++) {
-        const apartment = this.allApartaments[i];
-        const distance = this.distance(
-          this.lat,
-          this.lng,
-          apartment.lat,
-          apartment.lng
-        );
-        if (
-          distance <= this.distanceKm &&
-          apartment.room >= this.room &&
-          apartment.bed >= this.bed
-        ) {
-          // console.log("la distanza è: " + distance.toFixed(3) + " km :)");
-          // console.log(apartment.room);
-          // console.log(apartment.bed);
-          // console.log("Fatto");
-          this.correctApartments.push(apartment);
-        } else {
-        }
-        // console.log(this.correctApartments);
-      }
-    },
-    distance(lat1, lon1, lat2, lon2) {
-      if (lat1 == lat2 && lon1 == lon2) {
-        return 0;
-      } else {
-        var radlat1 = (Math.PI * lat1) / 180;
-        var radlat2 = (Math.PI * lat2) / 180;
-        var theta = lon1 - lon2;
-        var radtheta = (Math.PI * theta) / 180;
-        var dist =
-          Math.sin(radlat1) * Math.sin(radlat2) +
-          Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-        if (dist > 1) {
-          dist = 1;
-        }
-        dist = Math.acos(dist);
-        dist = (dist * 180) / Math.PI;
-        dist = dist * 60 * 1.1515;
-        dist = dist * 1.609344;
-        return dist;
-      }
-    },
   },
 };
 </script>
 
 <style scoped lang="scss">
 @import url("../../../public/css/layout-preset.css");
-
-// #Header{
-//     width: 100vw;
-//     height:10vh;
-//     display: flex;
-//     #logo-container{
-//         width: 15vw;
-//         height:10vh;
-//         display: flex;
-//         justify-content: center;
-//         align-items: center;
-//         img{
-//           width: 200px;
-//         }
-//     }
-//     #search-container{
-//         width: 70vw;
-//         height:10vh;
-//     }
-//     #btn-container{
-//         width: 15vw;
-//         height:10vh;
-//         display: flex;
-//         justify-content: center;
-//         align-items: center;
-//         a{
-//          margin: 1%;
-//         }
-//     }
-// }
 </style>
