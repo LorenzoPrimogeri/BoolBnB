e903950315f53117a448fd05227708029d89e8bf
michele camera <michy.cam95@gmail.com>
2022-07-08 10:23:13 +0200

    update we are page

--- resources/js/pages/WearePage.vue
+++ resources/js/pages/WearePage.vue
@@ -1,260 +1,247 @@
 <template>
-    <div class="body">
-        <div class="container pt-4">
-            <div class="card card0">
-                <div class="border">
-                    <h2>Michele Camera</h2>
-                    <div class="icons">
-                        <a class=""><img
-                                src="../../../public/img/linkedin-svgrepo-com.svg"
-                                alt="linkedin"></a>
+    <main>
+        <div class="container jc-c wmax-100">
+            <div class="cnt-cards">
+                <div class="cards box-card">
+                    <div class="picture">
+                        <img class="img-fluid" src="assets/img/photo.jpg">
                     </div>
-                </div>
-            </div>
-            <div class="card card1">
-                <div class="border">
-                    <h2>Lorenzo Primogeni</h2>
-                    <div class="icons">
-                        <i class="fa fa-codepen" aria-hidden="true"></i>
-                        <i class="fa fa-instagram" aria-hidden="true"></i>
-                        <i class="fa fa-dribbble" aria-hidden="true"></i>
-                        <i class="fa fa-twitter" aria-hidden="true"></i>
-                        <i class="fa fa-facebook" aria-hidden="true"></i>
+                    <div class="team-content">
+                        <h3 class="name">Lorenzo Primogeri</h3>
+                        <h4 class="title">Jr. FullStack Developer</h4>
+                        <h5>Backend Developer</h5>
                     </div>
+                    <ul class="social">
+                        <li><a href="#" class="ico ico-lnkd"
+                                aria-hidden="true"></a>
+                        </li>
+                    </ul>
                 </div>
-            </div>
-            <div class="card card2">
-                <div class="border">
-                    <h2>Francesco Moles</h2>
-                    <div class="icons">
-                        <i class="fa fa-codepen" aria-hidden="true"></i>
-                        <i class="fa fa-instagram" aria-hidden="true"></i>
-                        <i class="fa fa-dribbble" aria-hidden="true"></i>
-                        <i class="fa fa-twitter" aria-hidden="true"></i>
-                        <i class="fa fa-facebook" aria-hidden="true"></i>
-                    </div>
-                </div>
-            </div>
-            <div class="card card3">
-                <div class="border">
-                    <h2>Marco Cataldi</h2>
-                    <div class="icons">
-                        <i class="fa fa-codepen" aria-hidden="true"></i>
-                        <i class="fa fa-instagram" aria-hidden="true"></i>
-                        <i class="fa fa-dribbble" aria-hidden="true"></i>
-                        <i class="fa fa-twitter" aria-hidden="true"></i>
-                        <i class="fa fa-facebook" aria-hidden="true"></i>
-                    </div>
-                </div>
-            </div>
-            <div class="card card4">
-                <div class="border">
-                    <h2>Antonio Ruggiero</h2>
-                    <div class="icons">
-                        <i class="fa fa-codepen" aria-hidden="true"></i>
-                        <i class="fa fa-instagram" aria-hidden="true"></i>
-                        <i class="fa fa-dribbble" aria-hidden="true"></i>
-                        <i class="fa fa-twitter" aria-hidden="true"></i>
-                        <i class="fa fa-facebook" aria-hidden="true"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div>
 
+                <div class="cards box-card">
+                    <div class="picture">
+                        <img class="img-fluid" src="assets/img/photo.jpg">
+                    </div>
+                    <div class="team-content">
+                        <h3 class="name">Antonio Ruggero</h3>
+                        <h4 class="title">Jr. FullStack Developer</h4>
+                        <h5>Backend Developer</h5>
+                    </div>
+                    <ul class="social">
+                        <li><a href="#" class="ico ico-lnkd"
+                                aria-hidden="true"></a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="cards box-card">
+                    <div class="picture">
+                        <img class="img-fluid" src="assets/img/photo.jpg">
+                    </div>
+                    <div class="team-content">
+                        <h3 class="name">Francesco Moles</h3>
+                        <h4 class="title">Jr. FullStack Developer</h4>
+                        <h5>Backend Developer</h5>
+                    </div>
+                    <ul class="social">
+                        <li><a href="#" class="ico ico-lnkd"
+                                aria-hidden="true"></a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="cards box-card">
+                    <div class="picture">
+                        <img class="img-fluid" src="assets/img/photo.jpg">
+                    </div>
+                    <div class="team-content">
+                        <h3 class="name">Michele Camera</h3>
+                        <h4 class="title">Jr. FullStack Developer</h4>
+                        <h5>Fontend Developer</h5>
+                    </div>
+                    <ul class="social">
+                        <li><a href="#" class="ico ico-lnkd"
+                                aria-hidden="true"></a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="cards box-card">
+                    <div class="picture">
+                        <img class="img-fluid" src="assets/img/photo.jpg">
+                    </div>
+                    <div class="team-content">
+                        <h3 class="name">Marco Cataldi</h3>
+                        <h4 class="title">Jr. FullStack Developer</h4>
+                        <h5>Frontend Developer</h5>
+                    </div>
+                    <ul class="social">
+                        <li><a href="#" class="ico ico-lnkd"
+                                aria-hidden="true"></a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
         </div>
-    </div>
+    </main>
 </template>
 
 <script>
 export default {
     name: 'wearePage',
+
+    mounted() {
+
+        $(document).ready(function () {
+            let cards = $(".cards")
+            for (let i = 0; i < cards.length; i++) {
+                let target = Math.floor(Math.random() * cards.length - 1) + 1;
+                let target2 = Math.floor(Math.random() * cards.length - 1) + 1;
+                cards.eq(target).before(cards.eq(target2));
+            }
+        });
+    }
 }
+
 </script>
 
 <style scoped lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+//font-family: 'Lobster', cursive;
 
-.body {
-    height: 100vh;
-    width: 100vw;
-    background-color: #7174b6;
-
-}
-
-.box-title {
+main {
     display: flex;
-    justify-content: center;
-    margin-top: 3%;
-
-    h2 {
-        font-family: 'Lobster', cursive;
-        font-weight: bold;
-        font-size: 2.8rem;
-        color: #797cba;
-    }
-}
-
-.container {
-    display: flex;
-    flex-wrap: wrap;
-    height: 100vh;
-    width: 100vw;
-    max-height: 800px;
-    max-width: 1280px;
-    min-height: 600px;
-    min-width: 1000px;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    margin: 0 auto;
-}
-
-.border {
-    height: 369px;
-    width: 290px;
-    background: transparent;
-    border-radius: 10px;
-    transition: border 1s;
-    position: relative;
-}
-
-.border:hover {
-    border: 1px solid #fff;
-}
-
-.card {
-    height: 379px;
-    width: 300px;
-    background: #808080;
-    border-radius: 10px;
-    transition: background 0.8s;
-    overflow: hidden;
-    background: #000;
-    box-shadow: 0 70px 63px -60px #000;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-
-}
-
-.card0 {
-    background: url("../../../public/img/1618635419788.jpg")center center no-repeat;
-    background-size: 380px;
-}
-
-.card0:hover {
-    background: url("../../../public/img/1618635419788.jpg") left center no-repeat;
-    background-size: 600px;
-}
-
-.card0:hover h2 {
-    color: black;
-    opacity: 1;
-}
-
-.card0:hover .fa {
-    opacity: 1;
-}
-
-.card1 {
-    background: url("https://i.pinimg.com/originals/28/d2/e6/28d2e684e7859a0dd17fbd0cea00f8a9.jpg") center center no-repeat;
-    background-size: 300px;
-}
-
-.card1:hover {
-    background: url("https://i.pinimg.com/originals/28/d2/e6/28d2e684e7859a0dd17fbd0cea00f8a9.jpg") left center no-repeat;
-    background-size: 600px;
-}
-
-.card1:hover h2 {
-    opacity: 1;
-}
-
-.card1:hover .fa {
-    opacity: 1;
-}
-
-.card2 {
-    background: url("https://i.pinimg.com/originals/ee/85/08/ee850842e68cfcf6e3943c048f45c6d1.jpg") center center no-repeat;
-    background-size: 300px;
-}
-
-.card2:hover {
-    background: url("https://i.pinimg.com/originals/ee/85/08/ee850842e68cfcf6e3943c048f45c6d1.jpg") left center no-repeat;
-    background-size: 600px;
-}
-
-.card2:hover h2 {
-    opacity: 1;
-}
-
-.card2:hover .fa {
-    opacity: 1;
-}
-
-.card3 {
-    background: url("../../../public/img/marco\ c.jpg") center center no-repeat;
-    background-size: 380px;
-}
-
-.card3:hover {
-    background: url("../../../public/img/marco\ c.jpg") left center no-repeat;
-    background-size: 600px;
-}
-
-.card3:hover h2 {
-    opacity: 1;
-}
-
-.card3:hover .fa {
-    opacity: 1;
-}
-
-.card4 {
-    background: url("../../../public/img/antonio\ \(1\).jpg") center center no-repeat;
-    background-size: 380px;
-}
-
-.card4:hover {
-    background: url("../../../public/img/antonio\ \(1\).jpg") left center no-repeat;
-    background-size: 600px;
-}
-
-.card4:hover h2 {
-    opacity: 1;
-}
-
-.card4:hover .fa {
-    opacity: 1;
-}
-
-
-
-h2 {
-    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-    color: #fff;
-    margin: 20px;
-    opacity: 0;
-    transition: opacity 1s;
-}
-
-.fa {
-    opacity: 0;
-    transition: opacity 1s;
-}
-
-.icons {
-    position: absolute;
-    fill: #fff;
-    color: #fff;
-    height: 130px;
-    top: 226px;
-    width: 50px;
-    display: flex;
+    width: 100%;
+    margin: 180px auto;
     flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
+
+    .cnt-cards {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 25px;
+
+        .box-card {
+            display: flex;
+            flex-direction: column;
+            position: relative;
+            width: calc(100% / 4 - 25px);
+            padding: 30px 0 80px;
+            margin-bottom: 30px;
+            background-color: #d2d9ff;
+            text-align: center;
+            overflow: hidden;
+            border-radius: 20px;
+            border: 1px solid #dfdfdf;
+            -webkit-box-shadow: 0px 0px 10px 5px rgba(150, 150, 150, 0.2);
+            -moz-box-shadow: 0px 0px 10px 5px rgba(150, 150, 150, 0.2);
+            box-shadow: 0px 0px 10px 5px rgba(150, 150, 150, 0.2);
+
+            .picture {
+                margin: 0 auto;
+                position: relative;
+                display: inline-block;
+                height: 130px;
+                width: 130px;
+                margin-bottom: 20px;
+                z-index: 1;
+
+                &::before {
+                    content: "";
+                    width: 100%;
+                    height: 0;
+                    border-radius: 50%;
+                    background-color: #9eadff;
+                    position: absolute;
+                    bottom: 135%;
+                    right: 0;
+                    left: 0;
+                    opacity: 0.9;
+                    transform: scale(3);
+                    transition: all 0.3s linear 0s;
+                }
+
+                img {
+                    width: 100%;
+                    height: auto;
+                    border-radius: 50%;
+                    transform: scale(1);
+                    transition: all 0.9s ease 0s;
+                }
+            }
+
+            &:hover .picture::before {
+                height: 100%;
+            }
+
+            .picture img {
+                box-shadow: 0 0 0 5px #f7f5ec;
+                transform: scale(0.7);
+            }
+
+            .team-content {
+                display: flex;
+                flex-direction: column;
+                gap: 5px;
+
+                h3,
+                h5 {
+                    color: #3b426c;
+                }
+
+                h4 {
+                    color: #fff;
+                }
+            }
+
+            .title {
+                display: block;
+                font-size: 15px;
+                color: #4e5052;
+                text-transform: capitalize;
+            }
+
+            .social {
+                display: flex;
+                padding: 10px;
+                justify-content: center;
+                width: 100%;
+                margin: 0;
+                background-color: #313131;
+                position: absolute;
+                bottom: -100px;
+                left: 0;
+                transition: all 0.5s ease 0s;
+
+                li {
+                    display: inline-block;
+
+                    a {
+                        display: block;
+                        padding: 10px;
+                        font-size: 17px;
+                        color: white;
+                        transition: all 0.3s ease 0s;
+                        text-decoration: none;
+                    }
+
+                    .ico {
+                        width: 30px;
+                        height: 30px;
+                        background-color: lightgray;
+                    }
+
+                    .ico-lnkd {
+                        background: url("../../../public/img/linkedin-svgrepo-com.svg");
+                    }
+                }
+            }
+
+            &:hover .social {
+                bottom: 0;
+            }
+        }
+    }
 }
 </style>
