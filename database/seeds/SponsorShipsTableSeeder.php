4d86a147681f424c4ef7e81309421837d6beb157
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-07-11 12:18:52 +0200

    added something

--- database/seeds/SponsorShipsTableSeeder.php
+++ database/seeds/SponsorShipsTableSeeder.php
@@ -16,17 +16,20 @@ class SponsorShipsTableSeeder extends Seeder
             [
                 'type' => 'Gold',
                 'price' => 9.99,
-                'duration' => 144
+                'duration' => 144,
+                'img' => "uploads/ads-gold.svg"
             ],
             [
                 'type' => 'Silver',
                 'price' => 5.99,
-                'duration' => 72
+                'duration' => 72,
+                'img' => "uploads/ads-silver.svg"
             ],
             [
                 'type' => 'Bronze',
                 'price' => 2.99,
-                'duration' => 24
+                'duration' => 24,
+                'img' => "uploads/ads-bronze.svg"
             ],
         ];
         foreach ($sponsorships as $sponsorship) {
@@ -34,6 +37,7 @@ class SponsorShipsTableSeeder extends Seeder
             $newsponsorship->type = $sponsorship['type'];
             $newsponsorship->price = $sponsorship['price'];
             $newsponsorship->duration = $sponsorship['duration'];
+            $newsponsorship->img = $sponsorship['img'];
             $newsponsorship->save();
         }
     }
--- resources/views/admin/checkin.blade.php
+++ resources/views/admin/checkin.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 @section('content')
-    <div class="main-slct-item-card">
-        @foreach ($sponsors as $sponsor)
-            <div class="card">
-                <h2>{{ $sponsor->type }}</h2>
+<div class="main-slct-item-card">
+    @foreach ($sponsors as $sponsor)
+    <div class="card">
+        <img src="{{ asset('storage/' . $sponsor->img) }}" alt="">
+        <h2>{{ $sponsor->type }}</h2>
 
-                <span>Durata: {{ $sponsor->duration }} ore</span>
-                <div class="cnt-btn">
-                    <a
-                        href="{{ route('admin.checkout', ['sponsor_id' => $sponsor->id, 'apartment_id' => $apartment->id]) }}">
-                        <button>€ {{ $sponsor->price }}</button>
-                    </a>
-                </div>
-            </div>
-        @endforeach
+        <span>Durata: {{ $sponsor->duration }} ore</span>
+        <div class="cnt-btn">
+            <a href="{{ route('admin.checkout', ['sponsor_id' => $sponsor->id, 'apartment_id' => $apartment->id]) }}">
+                <button>€ {{ $sponsor->price }}</button>
+            </a>
+        </div>
     </div>
+    @endforeach
+</div>
 @endsection
